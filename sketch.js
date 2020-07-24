b55829ad018bc2f3a556683fa15dc16f2a8e6b05
kishan <govinkw9@protonmail.com>
2020-07-24 20:15:31 +0200

    sd

--- sketch.js
+++ sketch.js
@@ -37,8 +37,10 @@ function mousePressed() {
   let x = map(mouseX, 0, width, 0, 1);
   let y = map(mouseY, 0, height, 1, 0);
   console.log(x,y)
+  if (x > 1) or ( y > 1) ;{
   x_vals.push(x);
   y_vals.push(y);
+  }
 }
 
 function draw() {
