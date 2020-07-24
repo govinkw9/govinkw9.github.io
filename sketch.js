1aaa48625defc93495f712f69c2fc6ad52894abf
kishan <govinkw9@protonmail.com>
2020-07-24 20:10:44 +0200

    sd

--- sketch.js
+++ sketch.js
@@ -36,6 +36,7 @@ function predict(x) {
 function mousePressed() {
   let x = map(mouseX, 0, width, 0, 1);
   let y = map(mouseY, 0, height, 1, 0);
+  console.log(x,y)
   x_vals.push(x);
   y_vals.push(y);
 }
